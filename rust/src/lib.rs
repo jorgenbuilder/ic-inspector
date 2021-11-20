7fd80d431adadf8a5541181afe835d325bd29749
Jorgen <jorgen@buildnode.io>
2021-11-20 02:06:33 -0800

    simplify rust example

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn decode(x: Vec<u8>) -> Vec<u8> {
-  return x
-}
-
-#[wasm_bindgen]
-pub fn text(x: i32) -> i32 {
-  return x
+pub fn decode(input_buffer: &[u8]) -> u8 {
+  return 42
 }
