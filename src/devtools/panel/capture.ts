a425e0a856b8988c6047f128905a09516897ea8e
jorgenbuilder <jorgen@buildnode.io>
2022-05-19 17:34:40 -0700

    ignore certificate verification

--- src/devtools/panel/capture.ts
+++ src/devtools/panel/capture.ts
@@ -1,10 +1,13 @@
 // NOTE: Comments and code represent my best understanding. They are not authoritative and they are error prone.
 import * as Agent from '@dfinity/agent';
+import { RequestId, requestIdOf } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
 import CBOR from 'cbor';
 import { decode } from './candid';
 
 type CallType = 'query' | 'call' | 'read_state';
+
+let requestId : RequestId;
 
 export interface LogEvent {
     time: Date;
@@ -58,18 +61,19 @@ type DecodedCallResponse = null;
 
 // A decoded read_state response.
 interface DecodedReadStateResponse {
-    certificate: ArrayBuffer;
+    response: any;
 };
 
 const agent = new Agent.HttpAgent({ host: "https://ic0.app" });
+const localAgent = new Agent.HttpAgent({ host: "htt://localhost:8000" });
 
 // @ts-ignore
 const candidService = ({ IDL }) => IDL.Service({ __get_candid_interface_tmp_hack: IDL.Func([], [IDL.Text], ["query"]), });
 const actors: { [key: string]: Agent.Actor } = {};
 const candid: { [key: string]: [any, string | false] } = {};
-async function getActor(id: string): Promise<Agent.Actor> {
+async function getActor(id: string, isLocal: boolean = false): Promise<Agent.Actor> {
     if (actors[id] === undefined) {
-        actors[id] = Agent.Actor.createActor(candidService, { agent, canisterId: id });
+        actors[id] = Agent.Actor.createActor(candidService, { agent: isLocal ? localAgent : agent, canisterId: id });
         try {
             // @ts-ignore
             candid[id] = (await actors[id].__get_candid_interface_tmp_hack()) as string;
@@ -102,11 +106,14 @@ export default async function capture(
         return;
     };
 
+    const isLocal = event.request.url.includes('localhost');
+
     // Fetch the candid interface for this actor if we don't have it already.
-    await getActor(canister);
+    // await getActor(canister, isLocal);
 
     // NOTE: We're giving up on read_state for now. I don't see how we'll be able to decode these messages without stealing the agent across global scopes.
-    if (type === 'read_state') return;
+    // if (type === 'read_state') return;
+    const utfDecoder = new TextDecoder('utf-8');
 
     // Chrome erroneously parses our cbor into a string, so we need to back that out, then we can properly decode it as CBOR. (String -> B64 -> Bytes -> CBOR = Javascript Object)
     const bytes = _base64ToBytes(btoa(event?.request?.postData?.text as string));
@@ -116,34 +123,35 @@ export default async function capture(
     let request: DecodedQueryRequest | DecodedCallRequest | DecodedReadStateRequest;
     switch (type) {
         case 'query':
-            const d1 = data as Agent.QueryRequest;
-            const r1: DecodedQueryRequest = {
+            const queryData = data as Agent.QueryRequest;
+            const queryRequest: DecodedQueryRequest = {
                 type, host, canister,
-                method: d1.method_name,
-                payload: decodeDfinityObject(decode(d1.arg)),
-                caller: ((d1.sender as Principal)._isPrincipal ? d1.sender as Principal : Principal.fromUint8Array(d1.sender as Uint8Array)).toString(),
+                method: queryData.method_name,
+                payload: decodeDfinityObject(decode(queryData.arg)),
+                caller: ((queryData.sender as Principal)._isPrincipal ? queryData.sender as Principal : Principal.fromUint8Array(queryData.sender as Uint8Array)).toString(),
             };
-            request = r1;
+            request = queryRequest;
             break;
         case 'call':
-            const d2 = data as Agent.CallRequest;
-            const r2: DecodedCallRequest = {
+            const callData = data as Agent.CallRequest;
+            const callRequest: DecodedCallRequest = {
                 type, host, canister,
                 method: (data as Agent.CallRequest).method_name,
-                payload: decodeDfinityObject((decode(d2.arg))),
-                caller: ((d2.sender as Principal)._isPrincipal ? d2.sender as Principal : Principal.fromUint8Array(d2.sender as Uint8Array)).toString(),
+                payload: decodeDfinityObject((decode(callData.arg))),
+                caller: ((callData.sender as Principal)._isPrincipal ? callData.sender as Principal : Principal.fromUint8Array(callData.sender as Uint8Array)).toString(),
             };
-            request = r2;
+            requestId = requestIdOf(callData);
+            request = callRequest;
             break;
-        // case 'read_state':
-        //     const d3 = data as Agent.ReadStateRequest;
-        //     const r3 : DecodedReadStateRequest = {
-        //         type, host, canister,
-        //         caller  : (d3.sender as Principal)._isPrincipal ? d3.sender as Principal : Principal.fromUint8Array(d3.sender as Uint8Array),
-        //         x : d3.paths.map(([k, v]) => [utfDecoder.decode(k), '*Hashed address to retrieve response from IC output queue.*']),
-        //     };
-        //     request = r3;
-        //     break;
+        case 'read_state':
+            const readStateData = data as Agent.ReadStateRequest;
+            const readStateRequest: DecodedReadStateRequest = {
+                type, host, canister,
+                caller: ((readStateData.sender as Principal)._isPrincipal ? readStateData.sender as Principal : Principal.fromUint8Array(readStateData.sender as Uint8Array)).toString(),
+                x: readStateData.paths.map(([k, v]) => [utfDecoder.decode(k), '*Hashed address to retrieve response from IC output queue.*']),
+            };
+            request = readStateRequest;
+            break;
     };
 
 
@@ -161,22 +169,30 @@ export default async function capture(
             // Further decode response.
             switch (type) {
                 case 'query':
-                    const d1 = data as Agent.QueryResponse;
-                    const r1: DecodedQueryResponse = {
-                        status: d1.status,
-                        data: d1.status === 'replied' ? decodeDfinityObject(decode(d1?.reply?.arg)) : undefined,
+                    const queryData = data as Agent.QueryResponse;
+                    const queryResponse: DecodedQueryResponse = {
+                        status: queryData.status,
+                        data: queryData.status === 'replied' ? decodeDfinityObject(decode(queryData?.reply?.arg)) : undefined,
                     };
-                    response = r1;
+                    response = queryResponse;
+                    break;
+                case 'call':
+                    const callData = data as Agent.QueryResponse;
+                    const callResponse: DecodedQueryResponse = {
+                        status: callData.status,
+                        data: callData.status === 'replied' ? decodeDfinityObject(decode(callData?.reply?.arg)) : undefined,
+                    };
+                    response = callResponse;
                     break;
                 case 'read_state':
-                    const d2 = data as Agent.ReadStateResponse;
-                    const cert = new Agent.Certificate(d2, agent);
-                    // NOTE: cert.verify is the next step, but we can't use wasm in this chrome extension yet. Even after that, it seems probable we would need the signing agent to read the message, and I'm not sure if there's going to be a way to do pull that from the code running on the page outside the scope of this extension. Alternatively, we could give up on read_state messages, and just duplicate calls to create our own.
-                    console.log(cert);
-                    const r2: DecodedReadStateResponse = {
-                        certificate: d2.certificate,
+                    const readStateData = data as Agent.ReadStateResponse;
+                    const cert = new Agent.Certificate(readStateData, agent);
+                    const path = [new TextEncoder().encode('request_status'), requestId, 'reply'];
+                    
+                    const readStateResponse: DecodedReadStateResponse = {
+                        response: decode(cert.lookup(path) as ArrayBuffer)
                     };
-                    response = r2;
+                    response = readStateResponse;
                     break;
             };
         };
@@ -190,7 +206,6 @@ export default async function capture(
                 response: event.response,
             }
         };
-        console.log(entry);
 
         log.push(entry);
 
