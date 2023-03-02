f48f6e945389587bf9e1f202600ae9a81e07f1a4
jorgenbuilder <jorgen@buildnode.io>
2023-03-01 17:29:10 -0800

    feat: capture requests to icp-api.io

--- src/services/capture/select.ts
+++ src/services/capture/select.ts
@@ -1,5 +1,5 @@
 const boundaryNodeRegex =
-    /https?:\/\/(?:.+)?((?:ic0\.app|dfinity.network)|localhost:[0-9]+)\/api\/v2\/canister\/(.+)\/(query|call|read_state)/;
+    /https?:\/\/(?:.+)?((?:ic0\.app|dfinity.network|icp-api.io)|localhost:[0-9]+)\/api\/v2\/canister\/(.+)\/(query|call|read_state)/;
 
 /**
  * Determines whether a URL represents a request to an internet computer boundary node.
