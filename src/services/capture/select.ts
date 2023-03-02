a5f2090d886e413253d7d7f413e78c54d2b7e793
Jorgen <jorgen@buildnode.io>
2023-03-01 19:22:56 -0800

    feat: identify ic calls by header

--- src/services/capture/select.ts
+++ src/services/capture/select.ts
@@ -9,10 +9,21 @@ export function isBoundaryNodeURL(url: string): boolean {
 }
 
 /**
- * Determines whether a network event is an internet computer request/response that we want to record.
+ * Determines the presence of known ic-related headers in a network request. These headers have been found
+ * to be less reliable than the URL regex, but they do catch some cases that would otherwise be missed.
  */
-export function shouldCapture(event: chrome.devtools.network.Request) {
+export function hasICHeaders(event: chrome.devtools.network.Request): boolean {
+    return !!event.response.headers.find((h) => h.name.includes('x-ic-'));
+}
+
+/**
+ * Determines whether a network event is an internet computer request/response that we want to record.
+ * We rely solely on matching the url against a regex representing known boundary nodes. x-ic- headers
+ * proved less reliable.
+ */
+export function shouldCapture(event: chrome.devtools.network.Request): boolean {
     return (
-        isBoundaryNodeURL(event.request.url) && event.request.method === 'POST'
+        (isBoundaryNodeURL(event.request.url) || hasICHeaders(event)) &&
+        event.request.method === 'POST'
     );
 }
